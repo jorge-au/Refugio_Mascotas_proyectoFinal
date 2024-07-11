eda7d66f006546456789c6d47124d2d6ef0ccdff
Jorge <jorge.arivilca@gmail.com>
2024-07-11 14:02:34 -0500

    se corrigió la lógica del controlador login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,9 +1,9 @@
-const {  hashPassword} = require('../utils/handleHashPassword');
+const { hashPassword } = require('../utils/handleHashPassword');
 const pool = require('../dataBase/config');
 const bcrypt = require('bcrypt');
 
-module.exports={
-    formAdmin:(req, res) =>{
+module.exports = {
+    formAdmin: (req, res) => {
         res.render('pages/formAdmin')
     },
 
@@ -11,19 +11,19 @@ module.exports={
         res.render('pages/formLogin')
     },
 
-    registro: async(req, res) => {
+    registro: async (req, res) => {
         try {
-            const { usuario, contrasena } = req.body; 
-            
+            const { usuario, contrasena } = req.body;
+
             if (!usuario || !contrasena) {
                 return res.status(400).json({ msg: 'Usuario y contraseña son requeridos' });
             }
-    
+
             const hashedPassword = await hashPassword(contrasena);
-    
+
             const sql = 'INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)';
             const values = [usuario, hashedPassword];
-    
+
             pool.query(sql, values, (err, data) => {
                 if (err) {
                     throw err;
@@ -36,32 +36,51 @@ module.exports={
             res.status(500).json({ msg: 'Error en el servidor' });
         }
     },
-		
+
     login: async (req, res) => {
-        const { usuario, contrasena } = req.body;
-       const [valido] =  await pool.query(`SELECT * FROM usuarios WHERE usuario = ?`, usuario)
-       if(valido === undefined){
-         res.status(404).send('Usuario no encontrado')
-       }else if (!(await bcrypt.compare(contrasena, valido.contrasena))){
-        res.status(401).json({msg: 'No estás autorizado'})
-       }else {
-        req.session.userId = valido.id
-        res.redirect('/adoptantes')
-        // res.status(200).json({msg: 'ingreso exitoso'})
-       }
+        try {
+            const { usuario, contrasena } = req.body;
+            const [valido] = await pool.query(`SELECT * FROM usuarios WHERE usuario = ?`, [usuario]);
+            
+            if (!valido) {
+                return res.status(404).send('Usuario no encontrado');
+            }
+    
+            const passwordMatch = await bcrypt.compare(contrasena, valido.contrasena);
+            
+            if (!passwordMatch) {
+                return res.status(401).json({ msg: 'No estás autorizado' });
+            }
+    
+            let exito;
+            if (passwordMatch) {
+                exito = 1
+            } else {
+                exito = 0
+            }
+            const sql = 'INSERT INTO registros_inicio_sesion(id_usuario, fecha_hora, exito) VALUES(?, now(), ?)';
+            
+            pool.query(sql, [valido.id, exito], (err, data) => {
+                if (err) {
+                    return res.status(500).json({ msg: 'Error en el servidor' });
+                } else {
+                    req.session.userId = valido.id;
+                    return res.status(200).json({ msg: 'Ingreso exitoso' });
+                }
+            });
+        } catch (error) {
+            return res.status(500).json({ msg: 'Error en el servidor' });
+        }
+    
     },
 
     logout: async (req, res) => {
-		req.session.userId = null
-		res.redirect('formLogin')
-	}
+        req.session.userId = null
+        res.redirect('/')
+    }
 
-    
-      
-    
 
-}//Llave de cierre del module exports
+} //Llave de cierre del module exports
 
 
 
-      
